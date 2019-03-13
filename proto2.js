0ecf630db4976d5616daef230a61bec2237feb64
Kenzieboukis <46661901+Kenzieboukis@users.noreply.github.com>
2019-03-13 08:35:58 -0700

    init

---
+++ proto2.js
@@ -0,0 +1,36 @@
+console.log('reading.js');
+'use strict';
+
+var palestine = document.querySelector('#palestine');
+var israel = document.querySelector('#israel');
+
+israel.addEventListener('click', function(){
+ palestine.style.display='block';
+}
+)
+
+var faq = document.querySelector('#faq');
+var openfaq = document.querySelector('#openfaq');
+
+faq.addEventListener('click', function(){
+  openfaq.style.display='block';
+})
+
+// var story;
+//
+// function preload() {
+//   // load the sound
+//   story = loadSound("http://localhost:8000/Desktop/sonia.mp3");
+// }
+//
+// function setup() {
+//   story.play();
+// }
+//
+// function mousePressed() {
+//   if (story.isPlaying() ) {
+//     story.pause();
+//   } else {
+//     story.play();
+//   }
+// }
